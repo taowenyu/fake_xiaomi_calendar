3cf0d946458c9219b2686f4ed0fa98c63dfc7bd8
lsbbd <1206261187@qq.com>
2017-11-15 23:47:06 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style language="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-smoothing: antialiased;
+    font-smoothing: grayscale;
+  }
+</style>
---
+++ src/pages/Home/index.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div class="home">
+
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style lang="scss" src="./index.scss" scoped>
+
+</style>
