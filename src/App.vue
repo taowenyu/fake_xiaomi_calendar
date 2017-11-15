a545efa76866b1a5e9dcdce40961297ac48f206f
lsbbd <1206261187@qq.com>
2017-11-16 00:45:59 +0800

    完成日历样式

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,17 @@ export default {
 </script>
 
 <style language="scss">
+  * {
+    box-sizing: border-box;
+  }
+
   #app {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     font-smoothing: antialiased;
     font-smoothing: grayscale;
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,11 +1,43 @@
 <template lang="html">
   <div class="home">
-
+    <header>
+      <div class="today">
+        <div class="left">
+          <span class="month">11 月</span>
+        </div>
+        <div class="right">
+          <span class="year">2017 年</span>
+          <div class="wrapper">
+            <span class="day">15 日</span>
+            <span>今天<span class="caret">▾</span></span>
+          </div>
+        </div>
+      </div>
+      <div class="button">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+      </div>
+      <div class="calendar-header">
+        <span v-for="(item, index) in '日一二三四五六'.split('')">{{ item }}</span>
+      </div>
+    </header>
+    <div class="main">
+      <div class="day" v-for="i in 35">
+        <span class="num">
+          {{ i }}
+        </span>
+        <span class="nong">
+          初十
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
+
   }
 </script>
 
